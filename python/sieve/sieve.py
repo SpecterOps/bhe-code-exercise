297d0b7581e34a05331af34ffaefcddbc6ccd7be
muvvanikitha05 <131554981+muvvanikitha05@users.noreply.github.com>
2024-07-15 13:59:54 -0500

    code has added

--- python/sieve/sieve.py
+++ python/sieve/sieve.py
@@ -1,5 +1,16 @@
 class Sieve:
+    prime_numbers = []
+    limit = 200000000
+    arr = [0] * limit
     def __init__(self) -> None:
-        pass
+        for i in  range(2,self.limit):
+            if self.arr[i]!=0:
+                continue
+            self.prime_numbers.append(i)
+            # print(i)
+            for j in range(i,self.limit):
+                if i*j > self.limit-1:
+                    break
+                self.arr[i*j]+=1
     def nth_prime(self, n: int) -> int:
-        pass
+        return self.prime_numbers[n]
